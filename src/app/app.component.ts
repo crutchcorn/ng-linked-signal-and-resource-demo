1c7f9eabe404c094f8cbe75bd8440ae9982c37b1
Corbin Crutchley <git@crutchcorn.dev>
2024-10-18 02:25:12 -0700

    chore: more work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,15 +17,15 @@ import { getSpecificPokemon } from './services/specific-pokemon';
   template: `
     @defer (when isGameReady() && !pokeApi.isLoading()) {
       <p>What Pokemon has the ID of {{ currentId() }}?</p>
-      <input
-        min="0"
-        [max]="allPokemonApi.value()!.count"
-        [(ngModel)]="guess"
-        type="number"
-      />
-      @if (guess() !== -1) {
-        <p>Your guess is {{ guess() }}? Well...</p>
-        @if (guess() === currentId()) {
+      <select [(ngModel)]="guess">
+        <option value="">Select a Pokemon...</option>
+        @for (pokemon of allPokemonApi.value()!.results; track pokemon.name) {
+          <option value="{{ pokemon.url }}">{{ pokemon.name }}</option>
+        }
+      </select>
+      @if (guess() !== '') {
+        <p>Your guess is {{ userReadableGuess()!.name }}? Well...</p>
+        @if (isGuessCorrect()) {
           <p>That's right! It's {{ pokeApi.value()!.name }}</p>
         } @else {
           <p>Nope! It's {{ pokeApi.value()!.name }}</p>
@@ -37,7 +37,18 @@ import { getSpecificPokemon } from './services/specific-pokemon';
   `,
 })
 export class AppComponent {
-  guess = model(-1);
+  guess = model('');
+
+  userReadableGuess = computed(() => {
+    return this.allPokemonApi
+      ?.value()
+      ?.results.find((res) => res.url === this.guess());
+  });
+
+  isGuessCorrect = computed(() => {
+    if (!this.userReadableGuess()) return false;
+    return this.userReadableGuess()!.url.endsWith(String(this.currentId()!));
+  });
 
   allPokemonApi = resource({
     loader: () => {
